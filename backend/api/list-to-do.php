f85b3dacb42b72daaebb7ae2b81e0537c1d9a4a4
NicolaTeora <nikxodia@gmail.com>
2024-02-29 19:52:20 +0100

    feat : print in DOM

--- backend/api/list-to-do.php
+++ backend/api/list-to-do.php
@@ -1,15 +1,20 @@
 <?php 
 //versione breve per stampare
 
-# 1 recupero il file con i data che mi serve stampare
+# 1 recupero il file (formato stringa) con i data che mi serve stampare
 $json_file_list = file_get_contents('../data/datalist.json');
 
 /* VERSIONE ESTESA in caso:
-Qui va aggiunta la logica per un eventuale trattamento dei dati
+va aggiunta la logica per un eventuale trattamento dei dati
 es: (filtrare l'array)
+1 - trasformo
+    $array = json_decode($json_file_list)
+    function...ecc...
+2 - ri-trasformo
+    $file_json = json_encode($array)
 */
 
-# 2 recupero l'header per indicare in content type al browser
+# 2 recupero l'header per indicare il content type al browser
 header('Content-Type: application/json');
 
 # 3 stampo
--- frontend/main.js
+++ frontend/main.js
@@ -4,7 +4,25 @@ const app = createApp({
   data() {
     return {
       titleApp: "ToDo List",
+      list: [],
     };
+  },
+
+  //1 creo il metodo per la chiamata axios all'api
+  methods: {
+    fetchToDoList() {
+      axios
+        .get("http://localhost/php-todo-list-json/backend/api/list-to-do.php")
+        .then((response) => {
+          console.log(response.data.list);
+          this.list = response.data.list;
+        });
+    },
+  },
+
+  //2 interrogare l'api dopo averla creata nel backend e la richiamo all'apertura dell'app
+  mounted() {
+    this.fetchToDoList();
   },
 });
 
